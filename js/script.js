4d643d88a2cb13736ac9f3992fe1da05eb65cb27
Dragonborn69 <106514871+Dragonborn69@users.noreply.github.com>
2022-05-30 10:55:11 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.carousele').slick({
+      arrows: false,
+      autoplay: true
+    });
+  });
+
+  $(document).ready(function(){
+    $('.carousele1').slick({
+      arrows: false,
+      autoplay: true
+    });
+  });
